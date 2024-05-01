5e6011295d44ac7239e0b1829c21b5c43d37b05c
SalcRusso333 <tssmirsalruso@gmail.com>
2024-05-01 14:35:18 -0700

    Se creo un input y un boton

--- App.js
+++ App.js
@@ -1,21 +1,92 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from 'react';
+import { useState } from 'react';
+import { StyleSheet, Text, View, Button, TextInput, ScrollView, FlatList } from 'react-native';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Buenas buenas!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+  const [enteredGoalText, setEnteredGoalText] = useState('')
+  const [goals, setGoals] = useState([])
+  function handleInputGoal(enteredText){
+    console.log(enteredText)
+    setEnteredGoalText(enteredText)
+  }
+
+function handleAddGoal() {
+  console.log (enteredGoalText)
+  setGoals(()=>[...goals, {text: enteredGoalText, key: Math.random().toString()}])
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#FFE229',
-    alignItems: 'center',
-    justifyContent: 'center',
-    color: '#FFF29E'
-  },
-});
+  return(
+    <View style={styles.container}>
+      <View style={styles.inputContainer}>
+        <TextInput
+        style={styles.textInput}
+        placeholder='Your Goal!'
+        oneChangeText={handleInputGoal} />
+        <Button
+        title='Add Goal'
+        color={'#FFF636'}
+        onPress={handleAddGoal}
+        />
+        <View style={styles.goalsContiner}>
+          <FlatList
+          data={goals}
+          renderItem={ (itemData) => {
+
+            <View style={styles.goalsItem}>
+              <Text style={styles.goalsText}>
+            
+              </Text>
+              </View>
+          }}
+          >
+
+          </FlatList>
+        </View>
+      </View>
+    </View>
+  )
+}
+  const styles = StyleSheet.create({
+    container:{
+      flex: 1,
+      paddingTop: 50,
+      paddingHorizontal:20
+    },
+
+    imnputContainer:{
+      flex: 1,
+      flexDirection: 'row',
+      justifyContent: 'center',
+      alignItems: 'center',
+      marginBottom: 20,
+      borderBottomColor: '#FFF980'
+    },
+    btGoal:{
+      borderRadius: 20,
+      backgraundColor: '#F8FF6E',
+    },
+    textImput:{
+      borderWith: 1,
+      borderColor: '#ffffff',
+      width: '80%',
+      marginRight: 3,
+      padding: 8,
+      borderRadius: 5
+    },
+    goalsContiner: {
+      flex: 5,
+    },
+    goalsItem: {
+      margin: 8,
+      padding: 8,
+      borderRadius: 6,
+      backgroundColor: '#8576FF',
+      color: 'white'
+    },
+    goalsText: {
+      color: 'white'
+    }
+    
+
+    }
+  );
